3954e167a9eeefa2ed38d04b21581e97080d747e
angga0304 <angga0304@gmail.com>
2014-11-07 08:31:44 +0700

    coba ruby pertama

---
+++ Coba ruby/coba.rb
@@ -0,0 +1,10 @@
+class Coba
+  attr_accessor :nama, :berat
+  def initialize nama, berat
+    @nama = nama
+    @berat = berat
+  end
+end
+#orang = Coba.new
+#puts "Hello "+orang.nama
+
---
+++ Coba ruby/impcoba.rb
@@ -0,0 +1,5 @@
+require_relative "coba"
+puts "Masukan Nama"
+nama = gets
+
+
